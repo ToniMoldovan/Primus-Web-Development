8da622cc22c198c15bdff2192f0bbc2b6720a3d7
Toni <antonio.moldovan82@yahoo.com>
2022-02-05 02:45:11 +0200

    Finished header section, navbar & added JS timer. Also all the responsive part is finished.

---
+++ landingpage/assets/js/countdown.js
@@ -0,0 +1,30 @@
+const count = document.getElementById('count');
+
+const config = {
+  birthdate: 'Feb 18, 2022',
+  name: "Momo 🤍"
+};
+
+const second = 1000,
+  minute = second * 60,
+  hour = minute * 60,
+  day = hour * 24;
+
+let countDown = new Date(`${config.birthdate} 00:00:00`).getTime();
+
+x = setInterval(function() {
+  let now = new Date().getTime(),
+    distance = countDown - now;
+
+  document.getElementById('day').innerText = Math.floor(distance / day);
+  document.getElementById('hour').innerText = Math.floor(
+    (distance % day) / hour
+  );
+  document.getElementById('minute').innerText = Math.floor(
+    (distance % hour) / minute
+  );
+  document.getElementById('second').innerText = Math.floor(
+    (distance % minute) / second
+  );
+
+  })
